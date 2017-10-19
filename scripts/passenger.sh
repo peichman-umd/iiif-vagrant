9fc10610aaf4b22c977bb14369e4b74a8bfeea42
Peter Eichman <peichman@umd.edu>
2017-10-19 14:48:07 -0400

    LIBIIIF-31. Use the passenger yum repo.
    Requires installing the most recent version of the nss package to work. The nss library is the SSL implementation used by curl and libcurl on CentOS/RHEL.
    
    https://issues.umd.edu/browse/LIBIIIF-31

--- scripts/passenger.sh
+++ scripts/passenger.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-# https://www.phusionpassenger.com/library/install/apache/install/oss/tarball/
-# because Phusion's yum repo doesn't currently work with CentOS 7's curl
+# https://www.phusionpassenger.com/library/walkthroughs/deploy/ruby/ownserver/apache/oss/el7/install_passenger.html
 
-PASSENGER_VERSION=5.1.8
-PASSENGER_TGZ=/apps/dist/passenger-release-$PASSENGER_VERSION.tar.gz
-if [ ! -e "$PASSENGER_TGZ" ]; then
-    PASSENGER_PKG_URL=https://github.com/phusion/passenger/archive/release-${PASSENGER_VERSION}.tar.gz
-    curl -Lso "$PASSENGER_TGZ" "$PASSENGER_PKG_URL"
-fi
-tar xvzf "$PASSENGER_TGZ" --directory /apps
-
-PASSENGER_ROOT=/apps/passenger-release-$PASSENGER_VERSION
-
-$PASSENGER_ROOT/bin/passenger-install-apache2-module
-
-cat > /etc/profile.d/passenger.sh <<END
-export PATH=$PASSENGER_ROOT/bin:\$PATH
-END
-
-cp $PASSENGER_ROOT/buildout/apache2/mod_passenger.so /apps/iiif/apache/modules/
+yum install -y httpd epel-release pygpgme curl nss
+REPO_URL=https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo
+curl --fail -sSLo /etc/yum.repos.d/passenger.repo "$REPO_URL" && yum install -y mod_passenger
