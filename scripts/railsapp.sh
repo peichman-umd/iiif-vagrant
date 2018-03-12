0c6b4ea98cf4d6620eed3e0bcb1656e8742aaec2
Peter Eichman <peichman@umd.edu>
2018-03-12 14:41:22 -0400

    LIBIIIF-64. Removed unused files.
    * pcdm2manifest.yml
    * fcrepolocal.pem
    
    https://issues.umd.edu/browse/LIBIIIF-64

--- scripts/railsapp.sh
+++ scripts/railsapp.sh
@@ -4,7 +4,3 @@ source $HOME/.rvm/bin/rvm
 cd /apps/iiif/pcdm-manifests
 gem install bundler
 bundle install
-
-openssl s_client -connect fcrepolocal:443 \
-    | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' \
-    > /apps/iiif/pcdm-manifests/fcrepolocal.pem
